fb1902a9fd33771cf26d4baa2ce41acb3a2276f5
gürkan soykan <gsoykan@sabanciuniv.edu>
2018-05-06 04:15:43 +0300

    Initial commit of the WishI Project

---
+++ app/src/main/java/com/bucketsoft/user/wishi/MainActivity.java
@@ -0,0 +1,59 @@
+package com.bucketsoft.user.wishi;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.bucketsoft.user.wishi.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements WishItemFragment.OnListFragmentInteractionListener {
+
+    BottomNavigationView mBottomNavigationView;
+    private int mColumnCount = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBottomNavigationView = findViewById(R.id.navigation);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_item_home:
+                        selectedFragment = WishItemFragment.newInstance(mColumnCount);
+                        break;
+                    case R.id.action_item_feed:
+                        selectedFragment = WishItemFragment.newInstance(mColumnCount);
+                        break;
+                    case R.id.action_item_answer:
+                        selectedFragment = WishAnswerFragment.newInstance();
+                        break;
+                    case R.id.action_item_search:
+                        selectedFragment = WishSearchFragment.newInstance();
+                        break;
+                    case R.id.action_item_profile:
+                        selectedFragment = WisherProfileFragment.newInstance();
+                        break;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_frame_layout, selectedFragment);
+                transaction.commit();
+                return true;
+            }
+        });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame_layout, WishItemFragment.newInstance(mColumnCount));
+        transaction.commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+}
---
+++ app/src/main/java/com/bucketsoft/user/wishi/WishAnswerFragment.java
@@ -0,0 +1,38 @@
+package com.bucketsoft.user.wishi;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WishAnswerFragment extends Fragment {
+
+
+    public WishAnswerFragment() {
+        // Required empty public constructor
+    }
+
+    public static WishAnswerFragment newInstance() {
+        WishAnswerFragment fragment = new WishAnswerFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_wish_answer, container, false);
+        TextView textView = rootView.findViewById(R.id.wish_answer_question_text_view);
+        textView.setText("This is going to be a question, basically somebofy is looking for help!");
+        return rootView;
+    }
+
+}
